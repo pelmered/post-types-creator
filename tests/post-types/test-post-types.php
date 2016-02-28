6407250fbab3b9515698c6d730366fb686fc9142
Peter Elmered <peter@elmered.com>
2016-02-28 19:25:22 +0100

    Tests rewrite + new tests

---
+++ tests/post-types/test-post-types.php
@@ -0,0 +1,54 @@
+<?php
+
+class PostTypesTest extends WP_UnitTestCase {
+
+	static function setUpBeforeClass() {
+		require 'bootstrap-post-types.php';
+	}
+
+	function test_cpt_registered() {
+
+		$post_types = get_post_types();
+
+		$this->assertContains( 'stores', $post_types );
+		$this->assertContains( 'employees', $post_types );
+	}
+
+	function test_custom_post_status_exists() {
+
+		$post_statuses = get_post_stati();
+
+		$this->assertContains( 'active', $post_statuses );
+		$this->assertContains( 'completed', $post_statuses );
+	}
+
+	function test_insert_post(  )
+	{
+		$last_id = 0;
+
+		for( $i = 0; $i < 4; $i++ )
+		{
+			// Create post object
+			$my_post = array(
+				'post_title'    => 'random title',
+				'post_content'  => 'random text',
+				'post_status'   => 'active',
+				'post_type'		=> 'stores',
+				'post_author'   => 1,
+				'post_category' => array( 8,39 )
+			);
+
+			$post_id = wp_insert_post( $my_post );
+
+			if( $i != 0 )
+			{
+				// Post ID should be incremented by one for each successful insert
+				$this->assertEquals( $post_id,  ( $last_id + 1 ) );
+			}
+
+			$last_id = $post_id;
+		}
+	}
+
+}
+
---
+++ tests/taxonomies/test-taxonomies.php
@@ -0,0 +1,34 @@
+<?php
+
+class TaxonomiesTest extends WP_UnitTestCase {
+
+	static function setUpBeforeClass() {
+		require 'bootstrap-taxonomies.php';
+	}
+
+	function test_taxonomy_registered() {
+
+		$taxonomies = get_taxonomies();
+
+		$this->assertTrue( in_array('business_unit', $taxonomies) );
+	}
+
+	function test_insert_term( )
+	{
+		$last_id = 0;
+
+		for( $i = 0; $i < 4; $i++ )
+		{
+			$ids = wp_insert_term( 'random string'.$i, 'business_unit' );
+
+			if( $i != 0 )
+			{
+				// Term ID should be incremented by one for each successful insert
+				$this->assertEquals( $ids['term_id'],  ( $last_id + 1 ) );
+			}
+
+			$last_id = $ids['term_id'];
+		}
+	}
+}
+
